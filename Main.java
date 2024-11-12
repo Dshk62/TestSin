32e3c39cd59f1f323cc99b0384370c0fb0b248f4
Дарья Дмитриевна <150614719+Dshk62@users.noreply.github.com>
2024-11-12 11:07:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+class MySin
+{
+    double MSin(double x, int k)
+    {
+        double pi = Math.PI;
+        x = x % (2 * pi);
+
+    }
+    double ASin(double x, int k)
+    {
+        double pi = Math.PI;
+        x = x % (2 * pi);
+    }
+}
